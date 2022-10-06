c5fdeeac2b93890d6193dae8ed6eb760f9cea910
MuhammadSameh <muhammadsameh970@gmail.com>
2022-10-06 20:15:14 +0200

    Add Basket Controller

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<CartItem> Items { get; set; }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
