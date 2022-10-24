46020a45429117b963cafa176d260842dc8102aa
MuhammadSameh <muhammadsameh970@gmail.com>
2022-10-25 01:42:59 +0200

    [Feature] Discount Grpc

---
+++ src/Services/Discount/Discount.Grpc/Mapper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Mapper
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CouponModel,Coupon>().ReverseMap();
+        }
+    }
+}
