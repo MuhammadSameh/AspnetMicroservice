d5e3267293cdbe6f8c5e4b535e558939adca12a0
MuhammadSameh <muhammadsameh970@gmail.com>
2022-11-22 04:01:03 +0200

    Ordering Microservice

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcSerivce;
 using Basket.API.Repositries;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -12,10 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : Controller
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository;
+            _discountGrpcService = discountGrpcService;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -30,8 +33,12 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
-            
 
+            foreach (var item in basket.Items)
+            {
+                var discount  = _discountGrpcService.GetDiscount(item.ProductName).Amount;
+                item.Price -= discount;
+            }
             return Ok(await _repository.UpdateBasket(basket));
         }
 
---
+++ src/Services/Basket/Basket.API/GrpcSerivce/DiscountGrpcService.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcSerivce
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService;
+        }
+
+        public CouponModel GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return _discountProtoService.GetDiscount(discountRequest);
+        }
+    }
+}
