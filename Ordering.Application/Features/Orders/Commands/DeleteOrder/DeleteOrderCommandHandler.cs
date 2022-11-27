16214bbe07a1fd7dbe51fde2bbb1cac5eb7a723d
Ilead-NetTeam <nour.tarek@ileadco.com>
2022-11-27 13:22:58 +0200

    ApplicationServiceRegistration

--- Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -13,11 +13,21 @@ namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 {
     public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
     {
-        private IOrderRepository _orderRepository;
-        private IMapper _mapper;
-        private ILogger<DeleteOrderCommandHandler> _logger;
+        private readonly IOrderRepository _orderRepository;
+
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
         {
+
             var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
             if (orderToDelete == null)
                 _logger.LogError("Order Doesn't exist");
